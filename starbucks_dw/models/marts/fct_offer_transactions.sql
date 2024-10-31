91efded3510f5c5929e41c9c2ebb37c0ec85b438
paty-oliveira <dev.patricia.oliveira@gmail.com>
2024-10-31 20:00:30 +0000

    Add fct_offer_transactions model

---
+++ starbucks_dw/models/marts/fct_offer_transactions.sql
@@ -0,0 +1,60 @@
+{{
+  config(
+    materialized = 'table',
+    )
+}}
+
+{% set surrogate_key_columns = [
+    'transactions.offer_id',
+    'transactions.transaction_id',
+    'offers.channel'
+    ]
+%}
+
+with
+    offers as (
+        select offer_id, offer_type, channel, difficulty_rank, duration
+        from {{ ref('dim_offer') }}
+    ),
+
+    transactions as (
+        select
+            transaction_id,
+            offer_id,
+            transaction_type,
+            reward,
+            hours_since_start,
+            days_since_start
+        from {{ ref('stg_transcript') }}
+    ),
+
+    final as (
+        select
+            {{ dbt_utils.generate_surrogate_key(surrogate_key_columns) }}
+            as offer_transaction_key,
+            transactions.offer_id,
+            transactions.transaction_id,
+            offers.offer_type,
+            offers.channel as offer_channel,
+            offers.difficulty_rank as offer_difficulty_rank,
+            offers.duration as offer_duration,
+            transactions.reward as offer_reward,
+            case
+                when transactions.transaction_type = 'offer received'
+                then 'received'
+                when transactions.transaction_type = 'offer viewed'
+                then 'viewed'
+                when transactions.transaction_type = 'offer completed'
+                then 'completed'
+                when transactions.transaction_type = 'transaction'
+                then 'transaction'
+            end as transaction_status,
+            transactions.hours_since_start,
+            transactions.days_since_start,
+            current_timestamp as ingested_at
+        from transactions
+        inner join offers on transactions.offer_id = offers.offer_id
+    )
+
+select *
+from final
