7778a6046e918fa5e25c036567d7239e6064e0af
paty-oliveira <dev.patricia.oliveira@gmail.com>
2024-10-31 20:24:56 +0000

    Add fct_customer_transactions model

---
+++ starbucks_dw/models/marts/fct_customer_transactions.sql
@@ -0,0 +1,52 @@
+{{
+  config(
+    materialized = 'table',
+    )
+}}
+
+{% set surrogate_key_columns = [
+    'transactions.transaction_id',
+    'transactions.customer_id',
+    'transactions.offer_id'
+    ]
+%}
+
+with
+    customers as (
+        select customer_id, gender, age, income, subscribed_date
+        from {{ ref('stg_customer') }}
+    ),
+
+    transactions as (
+        select transaction_id, customer_id, offer_id, transaction_type
+        from {{ ref('stg_transcript') }}
+    ),
+
+    final as (
+        select
+            {{ dbt_utils.generate_surrogate_key(surrogate_key_columns) }}
+            as customer_transaction_key,
+            transactions.transaction_id,
+            transactions.customer_id,
+            transactions.offer_id,
+            customers.gender,
+            customers.age,
+            customers.income,
+            case
+                when transactions.transaction_type = 'offer received'
+                then 'received'
+                when transactions.transaction_type = 'offer viewed'
+                then 'viewed'
+                when transactions.transaction_type = 'offer completed'
+                then 'completed'
+                when transactions.transaction_type = 'transaction'
+                then 'transaction'
+            end as transaction_status,
+            customers.subscribed_date as customer_subscribed_date,
+            current_timestamp as ingested_at
+        from transactions
+        inner join customers on transactions.customer_id = customers.customer_id
+    )
+
+select *
+from final
