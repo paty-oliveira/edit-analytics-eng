0574020c08df68bfb602caa78a02786c98202f1c
Carla Ferreira <carla.s.ferreira@MacBook-Pro-de-Carla.local>
2024-12-07 16:46:18 +0000

    stg_profile tested

--- starbucks_dw/models/staging/stg_profile.sql
+++ starbucks_dw/models/staging/stg_profile.sql
@@ -8,15 +8,16 @@
 with
     transformed_profile as (
         select
-            a
-            , COALESCE(gender, "N/A") AS gender
+              a
+            , COALESCE(gender, 'N/A') AS gender
             , age
             , id AS customer_id
-            , CAST(became_member_on AS DATE) AS became_member_on
+            , TO_DATE(CAST(became_member_on AS TEXT), 'YYYYMMDD') AS became_member_on
             , COALESCE(income, 0) AS income
-            current_timestamp as ingested_at
+            , current_timestamp as ingested_at
         from {{ ref('profile') }}
     )
 
 select *
 from transformed_profile where age < 118
+order by a asc
