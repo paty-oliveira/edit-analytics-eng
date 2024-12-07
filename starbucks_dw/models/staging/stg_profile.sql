9bf5a6aabefe1165e1e2d380e0f34d3aff5c064a
Carla Ferreira <carla.s.ferreira@MacBook-Pro-de-Carla.local>
2024-12-07 16:02:34 +0000

    stg_profile dbt class

--- starbucks_dw/models/staging/stg_profile.sql
+++ starbucks_dw/models/staging/stg_profile.sql
@@ -1,1 +1,22 @@
 /* TO BE IMPLEMENTED BY THE STUDENT */
+{{
+  config(
+    materialized = 'view',
+    )
+}}
+
+with
+    transformed_profile as (
+        select
+            a
+            , COALESCE(gender, "N/A") AS gender
+            , age
+            , id AS customer_id
+            , CAST(became_member_on AS DATE) AS became_member_on
+            , COALESCE(income, 0) AS income
+            current_timestamp as ingested_at
+        from {{ ref('profile') }}
+    )
+
+select *
+from transformed_profile where age < 118
