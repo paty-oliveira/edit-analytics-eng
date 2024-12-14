57a8aeef8e86fc7153e146e41375a78d222377b5
DiogofCabral <diogofilipepereiracabral@gmail.com>
2024-12-14 11:54:09 +0000

    profile e schema

--- starbucks_dw/models/staging/stg_profile.sql
+++ starbucks_dw/models/staging/stg_profile.sql
@@ -1,1 +1,22 @@
-/* TO BE IMPLEMENTED BY THE STUDENT */
+
+{{
+  config(
+    materialized = 'view',
+    )
+}}
+
+with
+    transformed_profile as (
+        select
+            id as customer_id,
+            COALESCE(gender, 'N/A') AS gender,
+            age,
+            COALESCE(income, 0) AS income,
+            TO_DATE(became_member_on::TEXT, 'YYYYMMDD') AS become_member_date,
+            CURRENT_TIMESTAMP AS ingested_at
+        from {{ ref('profile') }}
+        where age >= 118
+    )
+
+select *
+from transformed_profile
