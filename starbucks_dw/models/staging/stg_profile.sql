a7dbe3f8a2bcfa55bdcaf01c768814ff97f41836
rutevicente <rute.s.v@protonmail.com>
2024-12-14 11:14:38 +0000

    dbt-run in 3 stg files

--- starbucks_dw/models/staging/stg_profile.sql
+++ starbucks_dw/models/staging/stg_profile.sql
@@ -1,1 +1,22 @@
-/* TO BE IMPLEMENTED BY THE STUDENT */
+{{
+  config(
+    materialized = 'view',
+    )
+}}
+
+
+with
+    transformed_profile as (
+        SELECT
+            id as customer_id,
+            coalesce(age, 0) as age,
+            coalesce(gender,'N/A') as gender,
+            coalesce(income,0) as income,
+            to_date(became_member_on::text,'YYYYMMDD') as became_member_date,
+            current_timestamp as ingested_at
+        FROM {{ ref('profile') }}
+        WHERE age > 1 and age < 118
+    )
+
+select *
+from transformed_profile
