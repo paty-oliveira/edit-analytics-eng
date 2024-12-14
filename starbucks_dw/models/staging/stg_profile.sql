e6cba6f0dcabbd90b883d3533a1b7e7592a7f365
rutevicente <rute.s.v@protonmail.com>
2024-12-14 12:29:12 +0000

    updated column name in stg_profile

--- starbucks_dw/models/staging/stg_profile.sql
+++ starbucks_dw/models/staging/stg_profile.sql
@@ -12,7 +12,7 @@ with
             coalesce(age, 0) as age,
             coalesce(gender,'N/A') as gender,
             coalesce(income,0) as income,
-            to_date(became_member_on::text,'YYYYMMDD') as subscribed_at,
+            to_date(became_member_on::text,'YYYYMMDD') as subscribed_date,
             current_timestamp as ingested_at
         FROM {{ ref('profile') }}
         WHERE age > 1 and age < 118
