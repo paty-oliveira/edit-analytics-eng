f04521e70d0740bc3bdaa3718563617e5ce9a4fa
Daniel Ducat <moraesd346@gmail.com>
2024-12-14 10:22:36 +0000

    Added stg_profile.sql

--- starbucks_dw/models/staging/stg_profile.sql
+++ starbucks_dw/models/staging/stg_profile.sql
@@ -1,1 +1,21 @@
 /* TO BE IMPLEMENTED BY THE STUDENT */
+{{
+  config(
+    materialized = 'view',
+    )
+}}
+
+with
+    transformed_profile as (
+        select
+            id as customer_id,
+            COALESCE(gender, 'N/A') as gender,
+            age,
+            TO_DATE(became_member_on::text, 'YYYYMMDD') as became_member_on,
+            COALESCE(income, 0) as income,
+            current_timestamp as ingested_at
+        from {{ ref('profile') }}
+    )
+
+select *
+from transformed_profile
