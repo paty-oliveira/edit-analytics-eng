3e6b6b0b7d71d2af67628d7e53aca504efe08400
Alunos <alunos@MacBook-Air-de-EDIT.local>
2024-12-14 11:03:36 +0000

    Adding stg tables

--- starbucks_dw/models/staging/stg_profile.sql
+++ starbucks_dw/models/staging/stg_profile.sql
@@ -1,1 +1,26 @@
 /* TO BE IMPLEMENTED BY THE STUDENT */
+{{
+  config(
+    materialized = 'view',
+    )
+}}
+
+with
+    transformed_profile as (
+        select
+            id as customer_id,
+            a,
+            COALESCE(gender, 'N/A') AS gender,
+            age,
+            TO_DATE(became_member_on::TEXT, 'YYYYMMDD') AS became_member_on,
+            COALESCE(income, 0) AS income,
+            CURRENT_TIMESTAMP AS ingested_at
+        from {{ ref('profile') }}
+        where age <> 118
+
+    )
+
+
+
+select *
+from transformed_profile
