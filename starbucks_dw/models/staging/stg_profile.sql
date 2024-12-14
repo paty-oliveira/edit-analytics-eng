58ef149a90eb4d73e808a28fa0b5c9d5782701b8
Carla Ferreira <carla.s.ferreira@MacBook-Pro-de-Carla.local>
2024-12-14 10:31:17 +0000

    Exercicio Pratico III and IV

--- starbucks_dw/models/staging/stg_profile.sql
+++ starbucks_dw/models/staging/stg_profile.sql
@@ -8,16 +8,15 @@
 with
     transformed_profile as (
         select
-              a
-            , COALESCE(gender, 'N/A') AS gender
+              COALESCE(gender, 'N/A') AS gender
             , age
             , id AS customer_id
             , TO_DATE(CAST(became_member_on AS TEXT), 'YYYYMMDD') AS became_member_on
             , COALESCE(income, 0) AS income
             , current_timestamp as ingested_at
         from {{ ref('profile') }}
+    where age <> 118
     )
 
 select *
-from transformed_profile where age < 118
-order by a asc
+from transformed_profile
